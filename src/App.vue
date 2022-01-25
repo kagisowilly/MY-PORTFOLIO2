91072694df572a63bf0e15f0ed0da3769fc70906
Kagisomphayi <mphayiw@gmail.com>
2022-01-25 16:32:10 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<Navbar />
+<Home/>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+import Home from './components/Home.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   Navbar, Home
   }
 }
 </script>
@@ -23,4 +25,96 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+/* Navbar Section*/
+
+.nav-link {
+  text-align: center;
+}
+.nav-link:hover {
+  color: rgb(255, 176, 6) !important;
+}
+
+.github:hover {
+  background-color: rgb(255, 176, 6) !important;
+  border-radius: 50%;
+}
+
+.netlify:hover {
+  background-color: rgb(255, 176, 6) !important;
+  border-radius: 50%;
+}
+.navbar {
+  top: 0;
+}
+
+.navbar-brand {
+  border-radius: 100%;
+  height: 100px;
+  width: 60x;
+  padding-bottom: 4px !important;
+  padding-top: 3px !important;
+  margin-top: 0px !important;
+  margin-bottom: 3px !important;
+}
+
+.one {
+  display: grid;
+  margin-top: 20px;
+  padding-top: 100px;
+  overflow-x: hidden !important;
+}
+
+ .num-one {
+  height: 100vh;
+  margin-top: 0;
+  background-image: url("./assets/images/Landing\ Section/background.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.7),
+      rgba(0, 0, 0, 0.7)
+    ),
+    url(./assets/images/Landing\ Section/background.jpg);
+  overflow-x: hidden;
+  background-position: center;
+}
+
+
+  /* BACKGROUND COLOR */
+  hr {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+  /* icons */
+  .icons {
+    height: 40px;
+    width: 40px;
+    line-height: 2;
+    border-radius: 100% !important;
+  }
+  
+  /* form {
+    width: 40% !important;
+  } */
+  .contactMe {
+    width: 100%;
+    margin: 0px;
+  }
+  .icons:hover {
+    color: rgb(255, 176, 6) !important;
+  }
+  
+  .button-about {
+    width: 25%;
+  }
+  
+  .arrow {
+    height: 40px;
+    width: 40px;
+  }
+
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+  <!-- LANDING SECTION -->
+  <section class="num-one" id="home">
+
+        <!-- Landing-->
+        <div class="container one">
+            <div class="row mt-3 pt-5">
+                <!-- <div class="col-lg-6 home-profile">
+                    <img src="https://images.unsplash.com/photo-1527526029430-319f10814151?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGRvZ3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+                        class="img-responsive" alt="...">
+                </div> -->
+
+                <div class="px-4 py-5 my-5 text-center col-">
+                    <h1 class="display-1 fw-bold text-warning mt-5 ">Kagiso Mphayi</h1>
+                    <div class="col-lg-6 mx-auto">
+                        <div class="lead text-white">
+                            <p class="mt-3 text-white"><span>| Juniour Frontend Deverloper |</span>
+                                <span>Juniour Designer |</span>
+                                <span>Photographer |</span>
+                            </p>
+
+                        </div>
+                        <div class="d-grid mt-4 gap-4 d-flex justify-content-center">
+                            <a target="_blank" href="https://github.com/Kagisomphayi"
+                                class="text-white border border-warning rounded icons "><i
+                                    class="bi icn bi-github"></i></a>
+                            <a target="_blank" href="https://www.linkedin.com/in/kagiso-kagiso-5aab71219/"
+                                class="text-white border border-warning rounded icons "><i
+                                    class="bi icn bi-linkedin "></i></a>
+                            <a target="_blank" href="https://twitter.com/K_Mphayi"
+                                class="text-white border-warning border rounded icons"><i
+                                    class="bi icn bi-twitter  "></i></a>
+                        </div>
+                        <div class="">
+                            <a href="#about" class="text-white rounded-pill "><button type="button"
+                                    class="rounded-pill mt-4 btn btn-sm btn-warning button-about">
+                                    <h4 class="text-black mb-0 abt">About me <br>
+                                    </h4><i class="bi bi-arrow-down-short"></i>
+                                </button></a>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section>
+
+
+    <div class="container hr">
+        <hr class=" bg-dark">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
